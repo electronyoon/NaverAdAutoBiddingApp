5ebe98f81753a9a68fa1bb28fd4e6580ceeb0ce5
Sangwoo Kim <electronyoon@gmail.com>
2023-06-16 12:14:49 +0900

    fixing betting threshold

--- util/getNewBid.js
+++ util/getNewBid.js
@@ -1,10 +1,12 @@
 export default function getNewBid(keywordRank, oldbid) {
     let betting;
-    // if (oldbid > 400) {
-    //     betting = 50;
-    // } else {
-    //     betting = 10;
-    // }
+    if (oldbid > 1000) {
+        betting = 100;
+    } else if (oldbid > 400) {
+        betting = 50;
+    } else {
+        betting = 10;
+    }
     betting = 10;
 
     if (keywordRank > 3)
